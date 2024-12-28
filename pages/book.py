18d4a7ef9ccb5c5d739e909d07c3e5fad276da2c
userness <auth.microsoft.no.reply@gmail.com>
2024-12-28 16:03:30 -0500

    Update book.py
    Made it work fr

--- pages/book.py
+++ pages/book.py
@@ -1,2 +1,13 @@
-import streamlit as st
-st.title("test")
+import streamlit as st                                         #https://google.com/search?q=text
+st.title("Digital Book Finder")
+
+with st.form("da-form"):
+	book = st.text_input("Book Title","The hunger Games 2")
+	st.caption("Enter To Save")
+
+	book = book.replace(" ", "+") # url encodinggggg i think
+	
+
+	submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.redirect(f'https://google.com/search?q={book}+doctype:pdf')
