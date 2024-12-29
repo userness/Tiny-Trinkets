d11589416bf520a12c3193ebab8cf24607254a67
userness <auth.microsoft.no.reply@gmail.com>
2024-12-28 21:32:31 -0500

    Update book.py
    Indentation is funky

--- pages/book.py
+++ pages/book.py
@@ -1,13 +1,10 @@
 import streamlit as st                                         #https://google.com/search?q=text
 st.title("Digital Book Finder")
-
 with st.form("da-form"):
 	book = st.text_input("Book Title","The hunger Games 2")
 	st.caption("Enter To Save")
-
 	book = book.replace(" ", "+") # url encodinggggg i think
 	
-
-    submitted = st.form_submit_button("Submit")	
-    if submitted:  
-        st.redirect(f'https://google.com/search?q={book}+filetype:pdf') 
+	submitted = st.form_submit_button("Submit")	
+	if submitted:  
+		st.redirect(f'https://google.com/search?q={book}+filetype:pdf')
